b7d75f1acce0a958ee26d606cb6bd30aeb563c10
MELEK <100144640+MELEK-GRL@users.noreply.github.com>
2025-06-17 12:09:08 +0300

    update

--- myapp/src/screen/Directive/index.jsx
+++ myapp/src/screen/Directive/index.jsx
@@ -201,7 +201,7 @@ const DirectiveList = () => {
             </div>
 
             {/* Liste */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
                 {filteredDirectives.map((item) => (
                     <ListCard key={item.id} item={item} />
                 ))}
